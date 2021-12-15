599ff83eb638671270d96878a4ac23833c00fed5
David Gwartney <david.gwartney@gmail.com>
2021-12-15 15:27:19 -0800

    Place holder for open weather api key

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 export FLASK_APP=rest
 export FLASK_ENV=development
 export FLASK_RUN_PORT=8000
+export OPEN_WEATHER_API_KEY=
 # TODO: check to see if enivronment is available
 conda activate flask-rest
